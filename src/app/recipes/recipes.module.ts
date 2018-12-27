b5f0b4f9117f695579b7ee7bdf877f6c0cb5d7f3
jbaisden <jasonbaisden@gmail.com>
2018-12-27 14:49:34 -0600

    added recipes module and changes needed for it

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeStartComponent,
+        RecipeDetailComponent,
+        RecipeEditComponent,
+        RecipeItemComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+        SharedModule
+    ]
+})
+export class RecipesModule {
+
+}
